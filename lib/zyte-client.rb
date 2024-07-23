6b931a38fead86406f8b65e16a98f75fe95c6057
leandrosardi <leandro.sardi@expandedventure.com>
2024-07-23 17:51:44 -0300

    parametrization of the old filename

--- lib/zyte-client.rb
+++ lib/zyte-client.rb
@@ -18,19 +18,19 @@ class ZyteClient
     # options: additonal Zyte API's options to the default request. Default request is: {"url": url}.
     # json_parsing: if true, the response is parsed as JSON using jq command, and return the value of the `httpResponseBody` key. Default is true.
     #
-    def extract(url:, json_parsing: true, options:{})
+    def extract(url:, json_parsing: true, options:{}, data_filename: 'zyte.data.json')
         data = {
             "url": url,
         }.merge(options)
-
-        File.open('zyte.data.json', 'w') { |file| file.write(data.to_json) }
+        
+        File.open(data_filename, 'w') { |file| file.write(data.to_json) }
 
         if json_parsing
             cmd = "curl \
             --silent \
             --user #{self.key}: \
             --header 'Content-Type: application/json' \
-            --data @zyte.data.json \
+            --data @#{data_filename} \
             --compressed \
             https://api.zyte.com/v1/extract \
             | jq --raw-output .httpResponseBody \
@@ -41,7 +41,7 @@ class ZyteClient
             --silent \
             --user #{self.key}: \
             --header 'Content-Type: application/json' \
-            --data @zyte.data.json \
+            --data @#{data_filename} \
             --compressed \
             https://api.zyte.com/v1/extract 
             "
